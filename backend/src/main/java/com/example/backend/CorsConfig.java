4a71349a729c2e5bf8ba81cec72281ebb15c05fc
Kevin <kevcourses0@gmail.com>
2025-04-27 18:50:15 -0500

    updated cors to use variable env

--- backend/src/main/java/com/example/backend/CorsConfig.java
+++ backend/src/main/java/com/example/backend/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("frontendURL")
+                        .allowedOrigins(frontendURL)
                         .allowedMethods("GET");
             }
         };
